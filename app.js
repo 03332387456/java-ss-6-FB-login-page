4810307de40fee1763b140d8a465e79c1fb52c97
03332387456 <sahirmavia@gmail.com>
2023-05-24 11:53:50 +0500

    facebook signup page and javascript asssignment 6

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+var a = document.getElementById("Firstinp")
+var b = document.getElementById("2ndinp")
+var c = document.getElementById("3rdinp")
+var d = document.getElementById("4thinp") 
+
+function signup() {
+    console.log(a.value);
+    console.log(b.value);
+    console.log(c.value);
+    console.log(d.value);
+    alert('thanks for information ')
+    alert('Now you have an Facebook account')
+}
